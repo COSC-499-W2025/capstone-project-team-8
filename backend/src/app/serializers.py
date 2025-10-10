c633c245c49fe22cedd1bfae1d543cb35c8d1e3c
Harper Kerstens <63266571+harperkerstens@users.noreply.github.com>
2025-10-09 20:41:15 -0700

    Initialize Django project structure with basic settings, URLs
    - Created manage.py for administrative tasks.
    - Added __init__.py to src directory.
    - Generated settings.py with default configurations for a new Django project.
    - Created urls.py for URL routing with an admin path.
    - Included necessary middleware and installed apps in settings.

---
+++ backend/src/app/serializers.py
@@ -0,0 +1,4 @@
+# This is where we define our serializers for converting complex data types, 
+# such as querysets and model instances, 
+# into native Python datatypes that can then be easily rendered 
+# into JSON, XML or other content types.
