60a92e792a929468a13f0127eb280f7d84575f91
Kyle Porter <kyle.porter04@pm.me>
2025-10-19 22:46:05 -0700

    feat: authentication backend
    - Signup api
    - Login api

--- src/backend/app/urls.py
+++ src/backend/app/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 from .views.uploadFolderView import UploadFolderView
+from .views.auth import LoginView, SignupView
 
 
 urlpatterns = [
     path("upload-folder/", UploadFolderView.as_view(), name="upload-folder"),
+    path("signup/", SignupView.as_view(), name="signup"),
+    path("login/", LoginView.as_view(), name="login"),
 ]
