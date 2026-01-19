0afb7d834ce55042462349248dd9c17f3e2abea4
Matin Raoufi <matinraoufi@outlook.com>
2026-01-19 11:45:02 -0800

    change migration id

--- src/backend/app/migrations/0010_remove_user_profile_image_url_project_thumbnail_and_more.py
+++ src/backend/app/migrations/0010_remove_user_profile_image_url_project_thumbnail_and_more.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ("app", "0008_user_degree_major_user_education_city_and_more"),
+        ("app", "0009_add_resume_model"),
     ]
 
     operations = [
