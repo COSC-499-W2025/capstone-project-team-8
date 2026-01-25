17e6a1523203a77da44fa1321d5b4a6a3912edc4
matin0014 <matinraoufi@outlook.com>
2026-01-24 23:57:18 -0800

    refactor(models): move ProgrammingLanguage and Framework to language.py and import in __init__.py

--- src/backend/app/models/__init__.py
+++ src/backend/app/models/__init__.py
@@ -4,4 +4,4 @@ from .user import User, UserManager
 from .project import Project, ProjectFile, ProjectLanguage, ProjectFramework, Contributor, ProjectContribution
 from .resume import Resume
 from .portfolio import Portfolio, PortfolioProject
-from ..models import ProgrammingLanguage, Framework
+from .language import ProgrammingLanguage, Framework
