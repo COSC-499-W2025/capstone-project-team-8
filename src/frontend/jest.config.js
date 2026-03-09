2262d3050459967f6b22a6e2642beca19bb254ee
matin0014 <matinraoufi@outlook.com>
2026-03-08 18:12:05 -0700

    add auto-generate resume utility

---
+++ src/frontend/jest.config.js
@@ -0,0 +1,9 @@
+/** @type {import('jest').Config} */
+module.exports = {
+  testEnvironment: 'node',
+  roots: ['<rootDir>/src'],
+  testMatch: ['**/__tests__/**/*.test.js'],
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1',
+  },
+};
